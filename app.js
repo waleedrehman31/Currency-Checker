dae7744ac16424ee2d820686f3df26c1effa8206
waleedrehman31 <waleedrehman31@gmail.com>
2022-08-24 15:46:03 +0500

    adding loading element and some UI changes

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const CURRENCY_API_KEY = config.CURRENCY_ACCESS_KEY;
 const CURRENCY_API_URI = `http://api.marketstack.com/v1/currencies?access_key=${CURRENCY_API_KEY}`;
 const currencyTable = document.getElementById("currency-table");
+const loadingElement = document.getElementById("loading");
+
+loadingElement.style.display = "none";
 
 async function getCurrency() {
-  const data = await fetch(CURRENCY_API_URI);
-  const JSON_DATA = await data.json();
-  JSON_DATA.data.forEach((currency, sNO = 1) => {
-    // console.log(currency);
-    currencyTable.innerHTML += `
+	loadingElement.style.display = "";
+	const data = await fetch(CURRENCY_API_URI);
+	const JSON_DATA = await data.json();
+	JSON_DATA.data.forEach((currency, sNO = 1) => {
+		// console.log(currency);
+		currencyTable.innerHTML += `
     <tr class="hover">
               <th>${++sNO}</th>
               <td>${currency.name}</td>
@@ -15,7 +19,8 @@ async function getCurrency() {
               <td>${currency.symbol}</td>
             </tr>
     `;
-  });
+	});
+	loading.style.display = "none";
 }
 
 getCurrency();
